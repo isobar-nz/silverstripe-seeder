f452a540a35f491a2ac51b651b7a2b389dd04863
Chris Barrett <christopherwalkerbarrett@gmail.com>
2015-10-21 10:29:29 +1300

    UPDATE: added missing namespace

--- code/Tasks/Unseed.php
+++ code/Tasks/Unseed.php
@@ -4,6 +4,7 @@ use LittleGiant\SilverStripeSeeder\CliOutputFormatter;
 use LittleGiant\SilverStripeSeeder\Util\BatchedSeedWriter;
 use Symfony\Component\Console\Application;
 use Symfony\Component\Console\Command\Command;
+use Symfony\Component\Console\Input\InputArgument;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Input\InputOption;
 use Symfony\Component\Console\Output\OutputInterface;
